997c4120a323f5f5ebdbfdfd0b63d1783c8adadc
Serkan062004 <serkan.sarigoz@gazi.edu.tr>
2023-12-25 23:25:37 +0300

    işlemler tamamlandı

---
+++ OkulAppSube1BIL/OkulApp.BLL/OgretmenBL.cs
@@ -0,0 +1,26 @@
+﻿using DAL;
+using OkulApp.MODEL;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace OkulApp.BLL
+{
+    public class OgretmenBL
+    {
+        public bool OgretmenEkle(Ogretmen ogrt)
+        {
+            SqlParameter[] p = {
+                             new SqlParameter("@Ad",ogrt.Ad),
+                             new SqlParameter("@Soyad",ogrt.Soyad),
+                             new SqlParameter("@Tckimlik",ogrt.Tckimlik)
+                         };
+
+            Helper hlp = new Helper();
+            return hlp.ExecuteNonQuery("Insert into tblOgretmenler values(@Ad,@Soyad,@Tckimlik)", p) > 0;
+
+        }
+    }
+}
+
---
+++ OkulAppSube1BIL/OkulAppSube1BIL/frmOgrtKayit.cs
@@ -0,0 +1,50 @@
+﻿using OkulApp.BLL;
+using OkulApp.MODEL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OkulAppSube1BIL
+{
+    public partial class frmOgrtKayit : Form
+    {
+        public frmOgrtKayit()
+        {
+            InitializeComponent();
+        }
+        private void btnKaydet_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                var obl = new OgretmenBL();
+                bool sonuc = obl.OgretmenEkle(new Ogretmen { Ad = txtAd.Text.Trim(), Soyad = txtSoyad.Text.Trim(), Tckimlik = txtTckimlik.Text.Trim() });
+                MessageBox.Show(sonuc ? "Ekleme Başarılı!" : "Ekleme Başarısız!!");
+            }
+            catch (SqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 2627:
+                        MessageBox.Show("Bu tc kimlikli öğretmen daha önce kayıtlı");
+                        break;
+                    default:
+                        MessageBox.Show("Veritabanı hatası");
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bilinmeyen Hata!!");
+            }
+        }
+    }
+    }
+
+
